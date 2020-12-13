5490ddd16b6f36d6b5a9f6773a029e4421f7d092
Michael George Chaniotakis <chaniotakismg@icloud.com>
2020-12-13 18:57:52 +0100

    Fix

--- basic/quantum_teleportation.py
+++ basic/quantum_teleportation.py
@@ -12,17 +12,16 @@ if __name__ == '__main__':
     circuit.cx(0,1)
     circuit.h(0)
     circuit.barrier()
-    circuit.measure([0,1], [0,1])
 
     # Quantum "Teleportation"
     circuit.barrier()
     circuit.cx(1,2)
     circuit.cz(0,2)
-    circuit.measure(2,2)
+    circuit.measure(2, 2)
 
     IBMQ.load_account()
     provider = IBMQ.get_provider(hub='ibm-q')
-    device = provider.get_backend('ibmqx2')
+    device = provider.get_backend('ibmq_santiago')
     job = execute(circuit, backend=device, shots=1024)
     print("Job ID:", job.job_id())
     job_monitor(job)
